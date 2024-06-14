c61185d20f13883bb0e81a06e141e6a75de08172
benskia <benski.abraham@gmail.com>
2024-06-14 15:58:29 -0500

    what is this

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func (cmd cliCommand) getCommandMap() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	return nil
+}
+
+func commandExit() error {
+	return nil
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex >")
+	}
+}
