03f2ddc70880b6027df150313b8569e356be5bd3
benskia <benski.abraham@gmail.com>
2024-06-17 13:05:54 -0500

    update Pokemon.PrintDetails formatting

--- internal/dex/dex.go
+++ internal/dex/dex.go
@@ -23,19 +23,19 @@ type Pokemon struct {
 
 func (p Pokemon) PrintDetails() {
 	fmt.Println()
-	fmt.Printf("Name: %v\n", p.Name)
-	fmt.Printf("Height: %v decimeters\n", p.Height)
-	fmt.Printf("Weight: %v hectograms\n", p.Weight)
+	fmt.Printf("Name:  %v\n", p.Name)
+	fmt.Printf("Height:  %v decimeters\n", p.Height)
+	fmt.Printf("Weight:  %v hectograms\n", p.Weight)
 	fmt.Print("Stats:\n")
-	fmt.Printf("\t-hp: %v\n", p.Stats.HP)
-	fmt.Printf("\t-attack: %v\n", p.Stats.Attack)
-	fmt.Printf("\t-defense: %v\n", p.Stats.Defense)
-	fmt.Printf("\t-special-attack: %v\n", p.Stats.SpecialAttack)
-	fmt.Printf("\t-special-defense: %v\n", p.Stats.SpecialDefense)
-	fmt.Printf("\t-speed: %v\n", p.Stats.Speed)
+	fmt.Printf("  -hp:  %v\n", p.Stats.HP)
+	fmt.Printf("  -attack:  %v\n", p.Stats.Attack)
+	fmt.Printf("  -defense:  %v\n", p.Stats.Defense)
+	fmt.Printf("  -special-attack:  %v\n", p.Stats.SpecialAttack)
+	fmt.Printf("  -special-defense:  %v\n", p.Stats.SpecialDefense)
+	fmt.Printf("  -speed:  %v\n", p.Stats.Speed)
 	fmt.Print("Types:\n")
 	for _, t := range p.Types {
-		fmt.Printf("\t- %v\n", t)
+		fmt.Printf("  - %v\n", t)
 	}
 	fmt.Println()
 }
